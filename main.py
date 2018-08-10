Implemented normalization to fix neuron saturation

main.py
@@ -1,40 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-import data
+import mnist
 import neural_network
 import sys
 import time
 
 def main():
 
-    print('Loading training data...')
+    print('Loading data...')
     start = time.time()
-    training_data = data.load_train('data')
-    end = time.time()
-    print('Time elapsed: %.2fs' % (round(end-start, 2)))
-    
-    print('Loading test data...')
-    start = time.time()
-    test_data = data.load_test('data')
+    training_data, test_data = mnist.load_data()
     end = time.time()
     print('Time elapsed: %.2fs' % (round(end-start, 2)))
     
     print('Initializing model...')
-    model = neural_network.NeuralNetwork((28*28, 30, 10))
+    model = neural_network.NeuralNetwork([784, 30, 10])
     
     print('Training model...')
     start = time.time()
-    model.stochastic_gradient_descent(training_data, test_data, 5, 10, 3)
+    model.stochastic_gradient_descent(training_data, test_data, 10, 10, 3.0)
     end = time.time()
     print('Time elapsed: %.2fs' % (round(end-start, 2)))
 
+    find_failures(model, test_data)
+
 def sample(data):
+    """ Plots a sample digit from a dataset. """
     X_sample, y_sample = data[random.randrange(len(data))]
     plt.gray()
     plt.imshow(np.reshape(X_sample, (28, 28)))
     plt.title('Label: %d' % (np.argmax(y_sample)))
     plt.show()
+
+def find_failures(model, data):
+    """ Finds and plots the digits that the model does not correctly classify. """
+
+    # find misclassified images
+    failures = [i for i, (x, y) in enumerate(data) if model.predict(x)!=y]
+
+    for index in failures:
+
+        # plot the image
+        plt.gray()
+        plt.imshow(np.reshape(data[index][0], (28, 28)))
+        plt.title('Label: %d' % (np.argmax(data[index][1])))
+        plt.show()
+
 
 if __name__ == '__main__':
     main()

neural_network.py
@@ -16,10 +16,6 @@ class NeuralNetwork:
         self.cost = quadratic_cost
 
     def stochastic_gradient_descent(self, training_data, test_data, epochs, mini_batch_size, alpha):
-
-        # combine separate x and y arrays to a list of (x, y) pairs
-        # training_data = [(x, y) for x, y in zip(x_train, y_train)]
-        # test_data = [(x, y) for x, y in zip(x_test, y_test)]
         
         # number of rows in dataset
         m = len(training_data)
@@ -32,7 +28,7 @@ class NeuralNetwork:
             # partition the data into mini-batches
             mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, m, mini_batch_size)]
 
-            # update weigths and biases for each mini-batch
+            # update weights and biases for each mini-batch
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, alpha)
 
@@ -59,8 +55,9 @@ class NeuralNetwork:
             b_gradient_batch = [bg_b + bg for bg_b, bg in zip(b_gradient_batch, b_gradient)]
 
         # update weights and biases according to learning rate alpha and the computed batch gradient
-        self.weights = [W - np.multiply((alpha/n), W_gradient) for W, W_gradient in zip(self.weights, W_gradient_batch)]
-        self.biases = [b - np.multiply((alpha/n), b_gradient) for b, b_gradient in zip(self.biases, b_gradient_batch)]
+        self.weights = [W - ((alpha/n) * W_gradient) for W, W_gradient in zip(self.weights, W_gradient_batch)]
+        self.biases = [b - ((alpha/n) * b_gradient) for b, b_gradient in zip(self.biases, b_gradient_batch)]
+
 
     def evaluate(self, test_data): # return the number of correctly classified data points
         test_results = [(self.predict(x)==np.argmax(y)) for x, y in test_data]
@@ -102,8 +99,8 @@ class NeuralNetwork:
         
         # propagate error backwards and compute gradients of all preceeding layers
         for l in range(2, self.num_layers):
-            delta = np.multiply(np.dot(self.weights[-l+1].transpose(), delta), self.activation(z_vals[-l], derivative=True))
-            W_gradient[-l] = np.dot(delta, a_vals[-l-1].transpose())
+            delta = np.dot(self.weights[-l+1].T, delta) * self.activation(z_vals[-l], derivative=True)
+            W_gradient[-l] = np.dot(delta, a_vals[-l-1].T)
             b_gradient[-l] = delta
 
         return W_gradient, b_gradient
@@ -111,11 +108,11 @@ class NeuralNetwork:
     def predict(self, x):
         return np.argmax(self.feedforward(x))
 
-def sigmoid(x, derivative=False):
+def sigmoid(z, derivative=False):
     if not derivative:
-        return 1/(1 + np.exp(-x))
+        return 1.0/(1.0 + np.exp(-z))
     else:
-        return np.multiply(sigmoid(x), sigmoid(1-x))
+        return np.multiply(sigmoid(z), sigmoid(1-z))
 
 def quadratic_cost(a, y, derivative=False):
     # sum of squares error